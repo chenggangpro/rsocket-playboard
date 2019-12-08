1108a9750b1e641e7230602135dad1e1740abae0
Cheng Gang <chenggangpro@gmail.com>
2019-12-09 00:19:43 +0800

    init project

---
+++ rsocket-playboard-server/src/test/java/pro/chengganng/rsocketplayboardserver/RsocketPlayboardServerApplicationTests.java
@@ -0,0 +1,11 @@
+package pro.chengganng.rsocketplayboardserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsocketPlayboardServerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
