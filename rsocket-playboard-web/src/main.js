85885257628ae83c9da205b49e197cd59fb7d9f7
Cheng Gang <chenggangpro@gmail.com>
2019-12-18 12:43:52 +0800

    update login page

--- rsocket-playboard-web/src/main.js
+++ rsocket-playboard-web/src/main.js
@@ -1,10 +1,39 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
-Vue.config.productionTip = false
+import vuetify from './plugins/vuetify';
+import rsocket from './plugins/rsocket';
+Vue.config.productionTip = false;
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
+
+rsocket().then((socket)=>{
+  socket.requestResponse({
+        data: {
+          "aaa": 123
+        },
+        metadata: {
+            route: "echo"
+        }
+      })
+      .subscribe({
+        onComplete: data => {
+          console.log("got response with requestResponse");
+          console.log(data)
+        },
+        onError: error => {
+          console.log("got error with requestResponse");
+          console.error(error);
+        },
+        onSubscribe: cancel => {
+          console.log(cancel)
+          /* call cancel() to stop onComplete/onError */
+        }
+      });
+}).catch(err=>{
+  console.error(err)
+});
---
+++ rsocket-playboard-web/src/views/Login.vue
@@ -0,0 +1,45 @@
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+    <v-app >
+        <v-content>
+            <v-container class="fill-height main_app" fluid >
+                <v-row align="center" justify="center">
+                    <v-col cols="12" sm="8" md="4">
+                        <v-card class="elevation-12">
+                            <v-toolbar color="primary" dark flat>
+                                <v-toolbar-title>Login</v-toolbar-title>
+                            </v-toolbar>
+                            <v-card-text>
+                                <v-form>
+                                    <v-text-field id="account" label="Account" name="account" prepend-icon="mdi-account" type="text"/>
+                                    <v-text-field id="password" label="Password" name="password" prepend-icon="mdi-lock" type="password"/>
+                                </v-form>
+                            </v-card-text>
+                            <v-card-actions>
+                                <v-spacer />
+                                <v-btn color="primary">Login</v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+    export default {
+        props: {
+            source: String,
+        },
+    }
+</script>
+
+<style>
+    .main_app{
+        background-image: url('../assets/login_background.jpg');
+        background-repeat:no-repeat;
+        background-size:100% 100%;
+        -moz-background-size:100% 100%;
+    }
+
+</style>
